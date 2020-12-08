c4a959249ac9a17cbfa7c733c508a43ffdfec660
Joao pedro Ceia <jceia@c1r1s1.42lisboa.com>
2020-12-08 09:17:08 +0000

    setting the first char as 0 to make strcat work

--- ex03/ft_strjoin.c
+++ ex03/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jceia <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 03:42:08 by jceia             #+#    #+#             */
-/*   Updated: 2020/12/08 06:20:43 by jceia            ###   ########.fr       */
+/*   Updated: 2020/12/08 09:16:13 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,9 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	while (index < size)
 		res_size += ft_strlen(strs[index++]);
 	result = malloc(res_size + 1);
+	if (result == NULL)
+		return (NULL);
+	result[0] = '\0';
 	index = 0;
 	while (index < size - 1)
 	{
