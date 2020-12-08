84a3df279e16d3c8e5c35fb8dd58ebc86bfd6a8c
Joao pedro Ceia <jceia@c1r1s1.42lisboa.com>
2020-12-08 09:14:51 +0000

    returning -1 in case of errors

--- ex02/ft_ultimate_range.c
+++ ex02/ft_ultimate_range.c
@@ -6,7 +6,7 @@
 /*   By: jceia <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 03:38:33 by jceia             #+#    #+#             */
-/*   Updated: 2020/12/05 03:41:24 by jceia            ###   ########.fr       */
+/*   Updated: 2020/12/08 09:12:30 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@ int	ft_ultimate_range(int **range, int min, int max)
 {
 	int	index;
 
+	if (range == NULL)
+		return (-1);
 	if (min < max)
 	{
 		*range = malloc(sizeof(**range) * (max - min));
+		if (*range == NULL)
+			return (-1);
 		index = 0;
 		while (min + index < max)
 		{
